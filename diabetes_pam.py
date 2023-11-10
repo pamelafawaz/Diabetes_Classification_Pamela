e5d5341b05b669f7678b4b5e369ecae66cc48248
pamelafawaz <144020030+pamelafawaz@users.noreply.github.com>
2023-11-10 14:32:53 +0100

    Update diabetes_pam.py

--- diabetes_pam.py
+++ diabetes_pam.py
@@ -10,14 +10,14 @@ loaded_scaler = joblib.load('scaler_diabetespam.pkl')
 st.title("Diabetes Classification App by Pamela")
 
 # Creating input fields for user data
-pregnancies = st.number_input("Number of Pregnancies", format='%f')
-glucose = st.number_input("Glucose Level", format='%f')
-blood_pressure = st.number_input("Blood Pressure", format='%f')
-skin_thickness = st.number_input("Skin Thickness", format='%f')
-insulin = st.number_input("Insulin Level", format='%f')
-bmi = st.number_input("BMI", format='%f')
-diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", format='%f')
-age = st.number_input("Age", format='%f')
+pregnancies = st.number_input("Number of Pregnancies", min_value=0.0, value=1.0, step=1.0)
+glucose = st.number_input("Glucose Level", min_value=0.0, value=100.0, step=1.0)
+blood_pressure = st.number_input("Blood Pressure", min_value=0.0, value=70.0, step=1.0)
+skin_thickness = st.number_input("Skin Thickness", min_value=0.0, value=20.0, step=1.0)
+insulin = st.number_input("Insulin Level", min_value=0.0, value=80.0, step=1.0)
+bmi = st.number_input("BMI", min_value=0.0, value=30.0, step=1.0)
+diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", min_value=0.0, value=0.5, step=0.01)
+age = st.number_input("Age", min_value=0.0, value=25.0, step=1.0)
 
 # Prediction button
 if st.button("Predict Diabetes"):
@@ -29,6 +29,7 @@ if st.button("Predict Diabetes"):
     prediction = loaded_model.predict(scaled_data)
 
     # Display the prediction
+    st.subheader("Prediction:")
     if prediction[0] == 1:
         st.write("The model predicts Diabetes.")
     else:
